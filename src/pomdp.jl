2e97a0cd44f7f9124141a118c12040c49123dcd9
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-09-24 17:59:16 -0700

    fixed bug where pomcp chose repeats

--- src/pomdp.jl
+++ src/pomdp.jl
@@ -4,7 +4,7 @@ function sparse_actions(pomdp::CommunityAffiliationPOMDP, s::CommunityNetwork, b
     @assert n <= 0 # doesn't work with a limited number of actions
     if s.budget > 0
         # println("ProbeNode actions (budget=$(s.budget))")
-        return [ProbeNode(i) for i in b.revealed_nodes]
+        return [ProbeNode(i) for i in setdiff(b.revealed_nodes,b.probed)]
     else
         # println("GuessAffiliation actions (budget=$(s.budget))")
         return [GuessAffiliation(i) for i in 1:b.n_comms]
